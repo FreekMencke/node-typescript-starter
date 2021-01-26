5c8f0224ba04f22dd2d4f1641f75363fc452b548
Jens Ungerer <github@jens-ungerer.de>
2021-01-26 04:57:12 +0100

    using arrow syntax instead of function syntax

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { App } from './app/app';
 import { Logger } from './app/common/logger';
 
-(function System() {
+const System = () => {
   Logger.instance.logTask(System.name, 'STARTING');
 
   App.run();
 
   Logger.instance.logTask(System.name, 'FINISHED');
-})();
+};
+System();
